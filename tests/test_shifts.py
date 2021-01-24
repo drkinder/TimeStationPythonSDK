83eeedbb41e80b8d7125b9a0d0cee43dbccf5f9d
Dylan <dylkinder@gmail.com>
2021-01-24 17:55:29 +0100

    first commit

---
+++ tests/test_shifts.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # self.ts = TimeStation()
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
